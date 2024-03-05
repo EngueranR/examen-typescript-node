8f02ff0bfe5fb810417ec8d35eae62e2c8daf619
engueranraout <rt.engueran@gmail.com>
2024-03-05 16:33:04 +0100

    exercice 4

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,28 @@ interface Person {
   age: number;
   height: number;
 }
+interface Statistics {
+  meanAge: number;
+  meanHeight: number;
+}
 
-function getStatistics(): number {
+function getStatistics(): Statistics {
   const persons: Person[] = JSON.parse(
     readFileSync("./persons.json").toString()
   );
-  const ages: number[] = persons.map((person) => person.age);
-  return Math.max(...ages);
+
+  let totalAge: number = 0;
+  let totalHeight: number = 0;
+
+  persons.forEach((person) => {
+    totalAge += person.age;
+    totalHeight += person.height;
+  });
+
+  return {
+    meanAge: totalAge / persons.length,
+    meanHeight: totalHeight / persons.length,
+  };
 }
 
 function displayResult() {
